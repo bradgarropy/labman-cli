06dcd8b475e9b74c6acaad0f2a3a874c4c4b9e71
Brad Garropy <bradgarropy@gmail.com>
2019-12-25 13:29:43 -0600

    logout console message. closes #21.

--- src/cli/logout.js
+++ src/cli/logout.js
@@ -1,11 +1,18 @@
 const conf = require("conf")
+const chalk = require("chalk")
 
 const config = new conf()
 
 const command = "logout"
 const description = "Remove GitHub credentials"
 const builder = {}
-const handler = () => config.clear()
+
+const handler = () => {
+    config.clear()
+    console.log(chalk.greenBright("\nLogout successful!\n"))
+
+    return
+}
 
 module.exports = {
     command,
